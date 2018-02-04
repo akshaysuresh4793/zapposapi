80e503383d4f4e0c0c28893cd092b0313925b3ad
Akshay Suresh <akshay.suresh.4793@gmail.com>
2018-02-04 04:45:39 -0500

    update schema

--- schema.sql
+++ schema.sql
@@ -2,28 +2,25 @@ CREATE DATABASE IF NOT EXISTS zappos;
 USE zappos;
 
 -- location
-CREATE TABLE location(
-	id int PRIMARY KEY AUTOINCREMENT,
-	name varchar2(500)
-)
+CREATE TABLE IF NOT EXISTS location(
+	id int PRIMARY KEY AUTO_INCREMENT,
+	name varchar(500));
 
 -- restaurant
-CREATE TABLE restaurant(
-	id int PRIMARY KEY AUTOINCREMENT,
-	name varchar2(500),
-	location_id REFERENCES location(id)
-)
+CREATE TABLE IF NOT EXISTS restaurant(
+	id int PRIMARY KEY AUTO_INCREMENT,
+	name varchar(500),
+	location_id int REFERENCES location(id));
 
 -- menu
-CREATE TABLE menu(
-	id int PRIMARY KEY AUTOINCREMENT,
-	name varchar2(500),
-	restaurant_id REFERENCES restaurant(id)
-)
+CREATE TABLE IF NOT EXISTS menu(
+	id int PRIMARY KEY AUTO_INCREMENT,
+	name varchar(500),
+	restaurant_id int REFERENCES restaurant(id));
 
 -- menu_item
-CREATE TABLE menu_item(
-	id int PRIMARY KEY AUTOINCREMENT,
-	name varchar2(500),
-	menu_id REFERENCES menu(id)
-)
+CREATE TABLE IF NOT EXISTS menu_item(
+	id int PRIMARY KEY AUTO_INCREMENT,
+	name varchar(500),
+	menu_id int REFERENCES menu(id)
+);
