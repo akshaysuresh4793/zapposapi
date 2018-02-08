5d2feb11f0a2e0a666ebac3ad25788f342564043
Akshay Suresh <akshay.suresh.4793@gmail.com>
2018-02-07 19:02:02 -0500

    add test db in schema

--- schema.sql
+++ schema.sql
@@ -33,3 +33,40 @@ CREATE TABLE IF NOT EXISTS menu_item(
 	menu_id int,
 	FOREIGN KEY(menu_id) REFERENCES menu(id),
 	INDEX(id));
+
+
+CREATE DATABASE IF NOT EXISTS zappos_test;
+USE zappos_test;
+
+-- location
+CREATE TABLE IF NOT EXISTS location(
+	id int PRIMARY KEY AUTO_INCREMENT,
+	name varchar(500),
+	INDEX(id));
+
+-- restaurant
+CREATE TABLE IF NOT EXISTS restaurant(
+	id int PRIMARY KEY AUTO_INCREMENT,
+	name varchar(500),
+	location_id int,
+	FOREIGN KEY(location_id) REFERENCES location(id),
+	INDEX(id));
+
+-- menu
+CREATE TABLE IF NOT EXISTS menu(
+	id int PRIMARY KEY AUTO_INCREMENT,
+	name varchar(500),
+	restaurant_id int,
+	FOREIGN KEY(restaurant_id) REFERENCES restaurant(id),
+	INDEX(id));
+
+-- menu_item
+CREATE TABLE IF NOT EXISTS menu_item(
+	id int PRIMARY KEY AUTO_INCREMENT,
+	name varchar(500),
+	description varchar(1000),
+	restaurant_id int,
+	FOREIGN KEY(restaurant_id) REFERENCES restaurant(id),
+	menu_id int,
+	FOREIGN KEY(menu_id) REFERENCES menu(id),
+	INDEX(id));
